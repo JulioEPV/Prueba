9baf90088fc9f5064d136d00009aa2da90c2894a
Julio Pinzon <je.pinzon@gicolombia.com>
2021-11-05 19:51:23 -0500

    Avances 5 NOV 21

--- pruebareact/src/App.js
+++ pruebareact/src/App.js
@@ -1,8 +1,9 @@
 import Index from "./pages/Index";
+import styles from "./styles/styles.css"
 
 function App() {
   return (
-    <div>
+    <div >
         <Index />
     </div>
   );
--- pruebareact/src/pages/Index.jsx
+++ pruebareact/src/pages/Index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Index = () => {
+    
+    const [variable1, setVariable1] = useState("Hola mundo, soy un estado");
+
+    const funcionClick = ()=>{
+        console.log("hice click en el boton")
+    }
+
     return (
         <div>
-            Hola mundo soy la pagina de index
+            <div className='fondo-verde'>
+                <span>El valor de la variable es:</span>
+                {variable1}
+            </div>
+            <input type ='text' placeholder= 'Ingrese el nuevo valor para la variable' />
+            <button onClick={funcionClick}>Haz click aca</button>
         </div>
     )
 }
