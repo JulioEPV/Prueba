8d204b9de663450f8fdd2a6044ea6ba1aff487b8
JulioEPV <je.pinzon@gicolombia.com>
2021-11-14 17:40:10 -0500

    Explicacion mongoose

---
+++ db/db.js
@@ -0,0 +1,13 @@
+import {connect} from 'mongoose';
+
+const conectarBD = async () =>{
+    return await connect(
+        'mongodb+srv://admin:AdminProyectos@gestionproyectosmisiont.9332r.mongodb.net/GestionProyectos?retryWrites=true&w=majority'
+    ).then(()=>{
+        console.log('Coenxion exitosa');
+    }).catch(e=>{
+        console.error('Error conectando a la bd', e);
+    });
+};
+
+export default conectarBD;
