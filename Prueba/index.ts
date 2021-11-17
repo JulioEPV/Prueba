46c29a2c8ad16ef55bca1c171a7cabfcd5236574
JulioEPV <je.pinzon@gicolombia.com>
2021-11-16 19:57:16 -0500

    Coleccion usuario + Creacion

--- Prueba/index.ts
+++ Prueba/index.ts
@@ -1,7 +1,20 @@
 import conectarBD from  './db/db';
+import { UserModel } from './models/user';
 
 const main  = async () => {
     await conectarBD();
+
+    // CREAR UN USUARIO
+/*     await UserModel.create({
+        correo:"dsl@c.com",
+        identificacion: "16546",
+        nombre: "Daniel",
+        apellido: "Saldarriaga",
+    }).then((u)=> {
+        console.log('usuario creado',u);
+    }).catch((e)=>{
+        console.error('Error creando el usuario',e);
+    }); */
 };
 
 main();
