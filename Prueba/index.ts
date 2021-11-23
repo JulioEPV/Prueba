bf598cf8bbb0a00b66917a6ee85703faa2c5fc77
JulioEPV <je.pinzon@gicolombia.com>
2021-11-22 21:47:06 -0500

    Explicacion objetivos

--- Prueba/index.ts
+++ Prueba/index.ts
@@ -13,17 +13,17 @@ const main  = async () => {
         tipo: Enum_TipoObjetivo.especifico,
     }) */
 
-    ProjectModel.create({
+/*     ProjectModel.create({
         nombre: 'Proyecto 2',
         presupuesto: 120,
         fechaInicio: Date.now(),
         fechaFin: new Date('2022/11/10'),
         lider:'6199943e581ebf8746f0fbdf',
         objetivos: ['619c3ae3d2a3b7ad95f5ec1e','619c3b89902b4a7477f5ced0']
-    });
+    }); */
 
-/*     const proyecto: any = await ProjectModel.find({nombre: 'Proyecto 2'}).populate('lider');
-    console.log('El proyecto es: ',proyecto); */
+    const proyecto: any = await ProjectModel.find({nombre: 'Proyecto 2'}).populate('objetivos');
+    console.log('El proyecto es: ',proyecto);
 
 /*     const lider = await UserModel.find({ _id: proyecto[0].lider});
     console.log('El lider del proyecto es: ',lider);
